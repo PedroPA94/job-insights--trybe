f5e41a3647b287f31f603d15672fefecb307dd25
Pedro Papini <ppapinia@gmail.com>
2023-02-16 16:46:11 -0300

    Resolve requisito 10

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,9 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    path = 'data/jobs.csv'
+    assert count_ocurrences(path, 'Javascript') == 122
+    assert count_ocurrences(path, 'javascript') == 122
+    assert count_ocurrences(path, 'Python') == 1639
+    assert count_ocurrences(path, 'python') == 1639
